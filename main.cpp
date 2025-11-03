ced4a1f6da96f0dddcc0ff062316d4cbbe07e7af
red-detroy <waisserberg@gmail.com>
2025-11-02 19:39:15 -0500

    More advanced main.cpp
    This expands main.cpp to handle data only so far; it will be expanded soon. after other cpps

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
 using namespace std;
 
+struct DataRow {
+    vector<double> features;
+    int label;
+};
+
+vector<DataRow> readCSV(string filename) {
+    ifstream file(filename);
+    vector<DataRow> data;
+    string line;
+    getline(file, line);
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string cell;
+        DataRow row;
+        int col = 0;
+
+        while (getline(ss, cell, ',')) {
+            double val = stod(cell);
+            if (col == 5)
+                row.label = (int)val;
+            else
+                row.features.push_back(val);
+            col++;
+        }
+        data.push_back(row);
+    }
+    return data;
+}
+
 int main() {
-    cout << "test." << endl;
+    vector<DataRow> data = readCSV("pokemon.csv");
+    cout << "Loaded " << data.size() << " rows." << endl;
     return 0;
 }
