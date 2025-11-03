4a9ba7b5776c8103a4140508143cb2b5f5190dca
ConmanTrialsOfKami <111887700+ConmanTrialsOfKami@users.noreply.github.com>
2025-11-03 16:41:04 -0500

    Main.cpp
    Simple syntax, and just adding exit option and while loop (3) lines of code ish.

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ vector<DataRow> readCSV(string filename) {
         int col = 0;
 
         while (getline(ss, cell, ',')) {
-            if (cell == "" || cell = "NA"){
+            if (cell == "" || cell == "NA"){
                 cell = "0";
             }
 
@@ -80,48 +80,53 @@ int main() {
     vector<DataRow> train;
     vector<DataRow> test;
     splitData(data, train, test);
+    while (true) {
+        cout << "Predict 'em All: Water-Proximity\n";
+        cout << "1. Decision Tree\n2. Naive Bayes\n3. Compare Both\n4. Exit\n";
+        cout << "Choose: ";
 
-    cout << "Predict 'em All: Water-Proximity\n";
-    cout << "1. Decision Tree\n2. Naive Bayes\n3. Compare Both\n";
-    cout << "Choose: ";
+        int choice;
+        cin >> choice;
 
-    int choice;
-    cin >> choice;
+        if (choice == 1) {
+            clock_t start = clock();
+            DecisionTree tree;
+            tree.train(train);
+            vector<int> preds = tree.predictAll(test);
+            clock_t end = clock();
+            cout << "\nDecision Tree Accuracy: " << accuracy(preds, test) << "%";
+            cout << "\nRuntime: " << double(end - start) / CLOCKS_PER_SEC << " sec\n";
+        }
+        else if (choice == 2) {
+            clock_t start = clock();
+            NaiveBayes nb;
+            nb.train(train);
+            vector<int> preds = nb.predictAll(test);
+            clock_t end = clock();
+            cout << "\nNaive Bayes Accuracy: " << accuracy(preds, test) << "%";
+            cout << "\nRuntime: " << double(end - start) / CLOCKS_PER_SEC << " sec\n";
+        }
+        else if (choice == 3) {
+            DecisionTree tree; tree.train(train);
+            NaiveBayes nb; nb.train(train);
 
-    if (choice == 1) {
-        clock_t start = clock();
-        DecisionTree tree;
-        tree.train(train);
-        vector<int> preds = tree.predictAll(test);
-        clock_t end = clock();
-        cout << "\nDecision Tree Accuracy: " << accuracy(preds, test) << "%";
-        cout << "\nRuntime: " << double(end - start) / CLOCKS_PER_SEC << " sec\n";
+            vector<int> preds1 = tree.predictAll(test);
+            vector<int> preds2 = nb.predictAll(test);
+
+            cout << "\nDecision Tree Accuracy: " << accuracy(preds1, test) << "%";
+            cout << "\nNaive Bayes Accuracy: " << accuracy(preds2, test) << "%\n";
+        }
+        else if (choice == 4) {
+            cout << "Goodbye!\n";
+            break;
+        }
+        else {
+            cout << "Invalid choice.\n";
+        }
+        cout << "\nPress Enter to exit...";
+        cin.ignore();
+        cin.get();
     }
-    else if (choice == 2) {
-        clock_t start = clock();
-        NaiveBayes nb;
-        nb.train(train);
-        vector<int> preds = nb.predictAll(test);
-        clock_t end = clock();
-        cout << "\nNaive Bayes Accuracy: " << accuracy(preds, test) << "%";
-        cout << "\nRuntime: " << double(end - start) / CLOCKS_PER_SEC << " sec\n";
-    }
-    else if (choice == 3) {
-        DecisionTree tree; tree.train(train);
-        NaiveBayes nb; nb.train(train);
-
-        vector<int> preds1 = tree.predictAll(test);
-        vector<int> preds2 = nb.predictAll(test);
-
-        cout << "\nDecision Tree Accuracy: " << accuracy(preds1, test) << "%";
-        cout << "\nNaive Bayes Accuracy: " << accuracy(preds2, test) << "%\n";
-    }
-    else {
-        cout << "Invalid choice.\n";
-    }
-    cout << "\nPress Enter to exit...";
-    cin.ignore();
-    cin.get();
     return 0;
 }
 
